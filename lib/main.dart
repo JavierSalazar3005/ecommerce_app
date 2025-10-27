00e575fc42b7bfd0426fd2df7752b037d6e74a3f
Javier Salazar <a2024110754@estudiantes.upsa.edu.bo>
2025-10-27 15:13:42 -0400

    create models

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,20 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const EcommerceApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class EcommerceApp extends StatelessWidget {
+  const EcommerceApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return MaterialApp(
+      title: 'E-Commerce UPSA',
+      theme: ThemeData(useMaterial3: true, colorSchemeSeed: Colors.blue),
+      home: const Placeholder(), // luego reemplazamos por LoginScreen()
+      debugShowCheckedModeBanner: false,
     );
   }
 }
+
---
+++ lib/models/order_item.dart
@@ -0,0 +1,23 @@
+class OrderItem {
+  final int productId;
+  final String productName;
+  int quantity;
+  final double unitPrice;
+  final int companyId;
+
+  OrderItem({
+    required this.productId,
+    required this.productName,
+    required this.quantity,
+    required this.unitPrice,
+    required this.companyId,
+  });
+
+  Map<String, dynamic> toJson() => {
+    "productId": productId,
+    "productName": productName,
+    "quantity": quantity,
+    "unitPrice": unitPrice,
+    "companyId": companyId,
+  };
+}
